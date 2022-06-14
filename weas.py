cc60eaae81454c5e71a46d768c92af2e1215353b
vicente romero <vbromero@miuandes.cl>
2022-06-14 03:46:49 -0400

    funcionan los if statements pero solo para printear al parecer ya que no puedo modificar los valores de las variables

--- weas.py
+++ weas.py
@@ -11,13 +11,23 @@ reserved={
    '(' : 'LPAREN',
    ')' : 'RPAREN',
    '[' : 'LBRACKET',
-   ']' : 'RBRACKET'
+   ']' : 'RBRACKET',
+   'si': 'IF',
+   'sino': 'ELSE'
 }
 
 tokens = [
     'N',
     'ID',
     'STRING',
+    'SSTRING',
+    'LIST',
+    'EQUAL',
+    'NOTEQ',
+    'LARGE',
+    'SMALL',
+    'LRGEQ',
+    'SMLEQ'
     ]+list(reserved.values())
 
 t_SU = r'\+'
@@ -29,6 +39,13 @@ t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_LBRACKET  = r'\['
 t_RBRACKET  = r'\]'
+
+t_EQUAL   = r'\=\='
+t_NOTEQ   = r'\!\='
+t_LARGE   = r'\>'
+t_SMALL   = r'\<'
+t_LRGEQ   = r'\>\='
+t_SMLEQ   = r'\<\='
 
 t_ignore = ' \t'
 
@@ -43,9 +60,15 @@ def t_ID(t):
     return t
 
 def t_STRING(t):
-    r'\"(\s*\w*\s*)\"'
+    r'\"(\s*\w*\s*)*\"'
     t.type = reserved.get(t.value,'STRING')
     return t
+
+def t_SSTRING(t):
+    r'\'(\s*\w*\s*)*\''
+    t.type = reserved.get(t.value,'SSTRING')
+    return t
+
 
 def t_error(t):
     print(":(")
@@ -67,9 +90,13 @@ def p_asignacion(t):
     'resultado : ID ASIGN s'
     variables[t[1]]=t[3]
 
-def p_expr_num_str(t):
-    '''s : N
-        | STRING'''
+def p_expr_str(t):
+    '''s : STRING
+        | SSTRING'''
+    t[0]=t[1][1:-1]
+
+def p_expr_num(t):
+    '''s : N'''
     t[0]=t[1]
     
 def p_expr_id(t):
@@ -87,10 +114,7 @@ def p_oper(t):
         |  s DIV s'''
     
     if t[2] == '+':
-        if type(t[1]) == type(t[3]) and type(t[1]) == str:
-            t[0] = str(t[1][:-1] + t[3][1:])
-        else:
-            t[0] = t[1] + t[3]
+        t[0] = t[1] + t[3]
     elif t[2] == '-':
         t[0] = t[1] - t[3]
     elif t[2] == '*':
@@ -100,6 +124,44 @@ def p_oper(t):
             t[0] = t[1] / t[3]
         except ZeroDivisionError:
             print('no podi dividir por 0 po oe')
+
+def p_comp(t):
+    '''comp : s EQUAL s
+          | s NOTEQ s
+          | s LARGE s
+          | s SMALL s
+          | s LRGEQ s
+          | s SMLEQ s'''
+    if t[2] == '==':
+        t[0] = t[1] == t[3]
+    elif t[2] == '!=':
+        t[0] = t[1] != t[3]
+    elif t[2] == '>':
+        t[0] = t[1] > t[3]
+    elif t[2] == '<':
+        t[0] = t[1] < t[3]
+    elif t[2] == '>=':
+        t[0] = t[1] >= t[3]
+    elif t[2] == '<=':
+        t[0] = t[1] <= t[3]
+
+def p_statement_if(p):
+    '''s : IF LPAREN comp RPAREN  s
+        |  IF LPAREN comp RPAREN  s  ELSE  s '''
+    if p[3]:
+        p[0] = p[5]
+        
+    else:
+        i=len(p)
+        if i >= 7:
+            print(p[7])
+            p[0] = p[7]
+
+def p_str(t):
+    '''s : STRING SU STRING
+        | SSTRING SU SSTRING'''
+    
+    t[0] = t[1][1:-1] + t[3][1:-1]
 
 def p_paren(t):
     '''s : LPAREN s SU s RPAREN
